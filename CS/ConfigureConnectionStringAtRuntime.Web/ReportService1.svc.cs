54d50ef0b613c0a13e996e963285283b78ae2e62
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-26 00:05:46 +0400

    Change example status to out of maintenance

--- CS/ConfigureConnectionStringAtRuntime.Web/ReportService1.svc.cs
+++
@@ -1,7 +0,0 @@
-﻿using DevExpress.Data.Utils.ServiceModel;
-
-namespace ConfigureConnectionStringAtRuntime.Web {
-    [SilverlightFaultBehavior]
-    public class ReportService1 : DevExpress.XtraReports.Service.ReportService {
-    }
-}
